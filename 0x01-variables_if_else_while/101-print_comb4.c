31d3eb4731eb25c2c5cb49888606d232c6b2d909
hollenaizer <avelounite@gmail.com>
2022-04-10 13:20:07 -0700

    Task 11 Advanced

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,13 +12,13 @@ for (firstD = 0; firstD < 8; firstD++)
 {
 for (secondD = firstD + 1; secondD < 9; secondD++)
 {
-for (thirdD = secondD +1; thirdD < 10; thirdD++)
+for (thirdD = secondD + 1; thirdD < 10; thirdD++)
 {
 putchar((firstD % 10) + '0');
 putchar((secondD % 10) + '0');
 putchar((thirdD % 10) + '0');
 
-if(firstD == 7 && secondD == 8 && thirdD == 9)
+if (firstD == 7 && secondD == 8 && thirdD == 9)
 continue;
 
 putchar(',');
