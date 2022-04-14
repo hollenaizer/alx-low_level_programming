9a4a2552bc7b2fb03d24a0436650936b6bcd79c4
hollenaizer <avelounite@gmail.com>
2022-04-14 16:27:19 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,15 +8,8 @@
 
 int main(void)
 {
-
-
-
 int p = 100;
-
 int i;
-
-
-
 i = 1;
 
 while (i <= p)
@@ -24,55 +17,29 @@ while (i <= p)
 {
 
 if (i % 3 == 0 && i % 5 == 0)
-
 {
-
 printf("FizzBuzz ");
-
 }
-
-
-
 else if (i % 3 == 0)
-
 {
-
 printf("Fizz ");
-
 }
-
-
 
 else if (i % 5 == 0)
-
 {
-
 if (i < p)
-
 printf("Buzz ");
-
 else
-
 printf("Buzz");
-
 }
-
-
-
 else
 
 {
-
 printf("%i ", i);
-
 }
-
-
-
 i++;
 
 }
-
 printf("\n");
 
 return (0);
