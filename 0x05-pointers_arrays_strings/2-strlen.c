a3087ce75bd88c8f40935ca905174c2614573792
hollenaizer <avelounite@gmail.com>
2022-04-20 12:57:06 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - len of a string
+ * @s: A pointer to an int 
+ *
+ * Return: void Correct
+ */
+
+int _strlen(char *s)
+{
+int u;
+u = 0;
+
+while(s[u] != '\0')
+{
+u++;
+}
+return (u);
+}
