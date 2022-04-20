459c1ba46a255ef4b62d17c1fc67cd84739281b8
hollenaizer <avelounite@gmail.com>
2022-04-20 12:41:42 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - updating an int value
+ * @n: a pointer to an int that will be updated
+ *
+ * Return: void always Correct
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
