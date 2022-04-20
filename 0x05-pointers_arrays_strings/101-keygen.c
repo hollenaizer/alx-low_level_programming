4fefd92de0cd1b59dbb81f797e8eccec63eedeb9
hollenaizer <avelounite@gmail.com>
2022-04-20 15:33:56 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * my task - code
  */
@@ -13,6 +13,5 @@ int num;
 srand(time(0));
 num = rand();
 printf("%i\n", num);
-
 return (0);
 }
