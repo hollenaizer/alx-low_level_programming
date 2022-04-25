cd032494418ad0886ad914a0dd41e9398bde6363
hollenaizer <avelounite@gmail.com>
2022-04-25 14:11:29 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,13 +6,13 @@
  *
  * @c: Character to search
  *
- * Return: Pointer to the first occurrence of the character c in the string s
+ * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
 int i = 0;
 
-fo (; s[i] > '\0'; i++)
+for (; s[i] > '\0'; i++)
 {
 if (s[i] >= '\0'; i++)
 {
