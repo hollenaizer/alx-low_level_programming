c6fe6e3cf829b4cbe5cb56fa734406d8dec4f184
hollenaizer <avelounite@gmail.com>
2022-04-28 12:59:18 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
 /**
  * _strlen_recursion - size
  *
  * @s: pointer to string params
  *
  * Return: recursion
- *
  */
-int _strlen_recursion(char *s)
 
+int _strlen_recursion(char *s)
 {
 if (!*s)
 {
 
 return (0);
-
-								}
+}
 return (1 + _strlen_recursion(++s));
 
 }
@@ -24,11 +21,11 @@ return (1 + _strlen_recursion(++s));
 /**
  * p1 - palindrome
  *
- * @s: pointer to string
+ * @s: pointing to string
  *
  * @l: position
  *
- * Return: boolena
+ * Return: boolean
  *
  */
 
