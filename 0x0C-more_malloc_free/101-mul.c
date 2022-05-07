0abd838866180a3c2fe79ee915228e0bb79e8d9d
hollenaizer <avelounite@gmail.com>
2022-05-06 18:41:41 -0700

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,15 +15,12 @@ unsigned long mul;
 int i, j;
 
 if (argc != 3)
-
 { 
 printf("Error\n");
 exit(98);
 }
 for (i = 1; i < argc; i++)
-
 {
-
 for (j = 0; argv[i][j] != '\0'; j++)
 {
 if (argv[i][j] > 57 || argv[i][j] < 48)
@@ -32,7 +29,6 @@ printf("Error\n");
 exit(98);
 }
 }
-
 
 }
 mul = atol(argv[1]) *atol(argv[2]);
